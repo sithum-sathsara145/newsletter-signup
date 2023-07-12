cd81c9410b48e2b115e450d6fe4350bfe1c40aaa
sithum-sathsara145 <sithumsathsara145@gmail.com>
2023-07-12 21:09:23 +0530

    created react routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Route,Routes} from "react-router-dom";
+import Newsletter from './components/Newsletter';
+import SuccessMsg from './components/SuccessMsg';
 
 export default function App() {
+
   return (
-    <div>App</div>
+    <div className='main__div'>
+      <Routes>
+        <Route path='/' element={<Newsletter/>}/>
+        <Route path='/success' element={<SuccessMsg/>}/>
+      </Routes>
+    </div>
   )
 }
