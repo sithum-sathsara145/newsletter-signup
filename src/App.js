b9f116b2462bb37f7c454684587e6c096a3f08c7
sithum-sathsara145 <sithumsathsara145@gmail.com>
2023-07-13 14:36:47 +0530

    created form validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { Route,Routes} from "react-router-dom";
-import Newsletter from './components/Newsletter';
-import SuccessMsg from './components/SuccessMsg';
-import './App.css'
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Newsletter from "./components/Newsletter";
+import SuccessMsg from "./components/SuccessMsg";
+import "./App.css";
+import { validateEmail } from "./Utils";
 
 export default function App() {
-
+  const [email, setEmail] = React.useState("");
+  const navigate = useNavigate();
+  const isValid = validateEmail(email);
   return (
-    <div className='app'>
+    <div className="app">
       <Routes>
-        <Route path='/' element={<Newsletter/>}/>
-        <Route path='/success' element={<SuccessMsg/>}/>
+        <Route
+          path="/"
+          element={
+            <Newsletter
+              email={email}
+              setEmail={setEmail}
+              navigate={navigate}
+              isValid={isValid}
+            />
+          }
+        />
+        {isValid && (<Route path="/success" element={<SuccessMsg />} />)}
+        
       </Routes>
     </div>
-  )
+  );
 }
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,28 +1,62 @@
 import React from "react";
-import "./newsletter.css";
-import newsletter from "../assets/images/illustration-sign-up-desktop.svg";
-import iconlist from '../assets/images/icon-list.svg'
 
-export default function Newsletter() {
+import newsletter from "../assets/images/illustration-sign-up-desktop.svg";
+import iconlist from "../assets/images/icon-list.svg";
+import { validateEmail } from "../Utils";
+
+export default function Newsletter({ email, setEmail, navigate }) {
+  const isValid = validateEmail(email);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail(email);
+    navigate("/success");
+  };
+  console.log(email, isValid);
   return (
     <div className="newsletter">
       <div className="newsletter-content">
         <h1>Stay updated!</h1>
-        
+
         <p>
           Join 60,000+ product managers receiving monthly updates on:
           <div className="ticklist">
-            <div> <img src={iconlist} alt="tick"  /> Product discovery and building what matters</div>
-            <div><img src={iconlist} alt="tick"  />Measuring to ensure updates are a success </div>
-            <div><img src={iconlist} alt="tick"  />And much more!</div>
+            <div>
+              {" "}
+              <img src={iconlist} alt="tick" /> Product discovery and building
+              what matters
+            </div>
+            <div>
+              <img src={iconlist} alt="tick" />
+              Measuring to ensure updates are a success{" "}
+            </div>
+            <div>
+              <img src={iconlist} alt="tick" />
+              And much more!
+            </div>
           </div>
         </p>
-        <form >
-          <label htmlFor="email" >Email address</label>
-          <input type="email" name="email" id="email"  placeholder="email@company.com"/>
-          <button type="submit">Subscribe to monthly newsletter</button>
+        <form>
+          <div>
+            <label htmlFor="email">Email address</label>
+            {(email === '' || !isValid) && (
+              <p>valid email required</p>
+            )
+            }
+          </div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email@company.com"
+            required
+            className={(email === '' || !isValid)&& ('valid')}
+          />
+          <button type="submit" onClick={handleSubmit} >
+            Subscribe to monthly newsletter
+          </button>
         </form>
-         
       </div>
       <div className="newsletter-image">
         <img src={newsletter} alt="newsletter" width="300px" />
